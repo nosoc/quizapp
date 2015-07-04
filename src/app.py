dd049d3a67139ba9fef46bf6fba017f1f4108e13
khdaria <kharkina.daria@gmail.com>
2015-07-04 16:34:10 +0300

    Что-то произошло

--- src/app.py
+++ src/app.py
@@ -42,8 +42,9 @@ def mathes_handler():
 @login_required
 def match_handler(match_id):
     if request.method == 'GET':
-        # get match questions
-        pass
+        match = app.db[MATCHES].find({'_id': match_id}).limit(1))
+        return: render_template("match.html", match_id=match)
+
     if request.method == 'POST':
         # finish the match
         pass
