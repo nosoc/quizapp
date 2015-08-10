b5e073eb176d1c61e0e43419d6a1a1cb51ae07ef
khdaria <kharkina.daria@gmail.com>
2015-08-10 21:30:47 +0300

    Загрузка вопросов

--- src/loading_questions.py
+++ src/loading_questions.py
@@ -3,14 +3,14 @@ from pymongo import MongoClient
 
 DATABASE_NAME = "summertime_webness"
 
-questions = pandas.read_csv("C:/Users/1/Desktop/quiz_questions.csv", sep=";", encoding="windows-1251")
+questions = pandas.read_csv("C:/Users/1/Desktop/quiz_questions.csv", sep=",", encoding="UTF-8")
 
 questions_dict = []
 
 for index, row in questions.iterrows():
     d = {'question': row["lyrics"],
          'answers': {'a': row["artist_true"], 'b': row["artist_false_1"],
-         'c': row["artist_false_2"], 'd': row["artist_false_3"]}, 'correct_answer': row["correct"]}}
+         'c': row["artist_false_2"], 'd': row["artist_false_3"]}, 'correct_answer': row["correct"]}
     questions_dict.append(d)
 
 db = MongoClient('wardoctor.nosoc.io', port=443)[DATABASE_NAME]
